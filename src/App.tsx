e0000394bd1cf67dda5b4835fca5f4dc6040d2b2
Martins Gouveia <gouveiamartins6@gmail.com>
2022-07-27 14:06:42 +0100

    🔇 removido comentários

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,6 @@
-import { Routes, Route, Link } from "react-router-dom";
 import { Router } from "./router/Router";
-import { Home } from "./components/Home";
-import { DigimonList } from "./components/DigimonList";
-
 import "./style/App.css";
-
-/*
-    #Ao acessar a primeira página ele deve digitar um nome e clicar em ver cartas.
-    #Na tela seguinte ele visualiza as 5 cartas e o  seu nome no canto superior direito.
-*/
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
 	return (
